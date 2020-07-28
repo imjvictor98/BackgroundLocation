b5f56a9b75ae70e18d6a49b777689b3a1e285926
imjvictor98 <jv.camara@hotmail.com>
2020-07-28 10:58:57 -0300

    Initial commit

---
+++ app/src/main/java/com/joaovictor/backgroundlocation/SendLocationToActivity.java
@@ -0,0 +1,19 @@
+package com.joaovictor.backgroundlocation;
+
+import android.location.Location;
+
+class SendLocationToActivity {
+    private Location location;
+
+    public SendLocationToActivity(Location mLocation) {
+        this.location = mLocation;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
